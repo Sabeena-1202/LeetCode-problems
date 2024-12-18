20a83bf7b88dec62c6c4b9d5d018ce1910a54d3b
Sabeena-1202 <sabeenamuki@gmail.com>
2024-12-18 16:01:36 +0530

    [1053] [Time Beats: 100.00%] [Memory Beats: 73.21%] - LeetPush

---
+++ 1053-Previous-Permutation-With-One-Swap.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] prevPermOpt1(int[] arr) {
+        int i=arr.length-2;
+        while(i>=0 && arr[i]<=arr[i+1]){
+            i--;
+        }
+        if(i<0){
+            return arr;
+        }
+            int j=arr.length-1;
+            while(j>i && (arr[j]>=arr[i]|| arr[j]==arr[j-1])){
+                j--;
+            }
+    
+            int temp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+    return arr;        
+
+}
+}
